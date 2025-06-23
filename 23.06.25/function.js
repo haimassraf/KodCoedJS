0d02c5270d1ba2cf93cc355f26dc6f2ce82cceac
Haim Assraf <haimassraf@gmail.com>
2025-06-23 13:09:25 +0300

    fd

--- 23.06.25/function.js
+++ 23.06.25/function.js
@@ -1,20 +1,29 @@
-function start(num, fun){
+function start(num, fun) {
    return fun(num, 2)
 }
 
-const multiply = (num1, num2) =>  {
+const multiply = (num1, num2) => {
    res = num1 * num2
    return subtract(res, 5)
-} 
+}
 
-const subtract = (num1, num2) =>  {
+const subtract = (num1, num2) => {
    res = num1 - num2
    return divide(res, 10)
-} 
+}
 
-const divide = (num1, num2) =>  num1 / num2 
+const divide = (num1, num2) => num1 / num2
 
-const add = (num1, num2) =>  num1 + num2 
+const add = (num1, num2) => num1 + num2
 
 res = start(5, multiply)
-console.log(res);
+// console.log(`res = ${res}`);
+
+// block scope
+function test() {
+   let x = 5;
+   if (true) {
+      let x = 10;
+   }
+   console.log(x);
+}              
