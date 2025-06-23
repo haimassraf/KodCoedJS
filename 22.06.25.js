9f5985e2c31c780ccf05341dda239050e0526ec8
Haim Assraf <haimassraf@gmail.com>
2025-06-23 11:55:01 +0300

    function

--- 22.06.25.js
+++ 22.06.25.js
@@ -3,27 +3,27 @@ let a = "first string";
 let b = "secound string";
 a, b = [b, a];
 console.log(a);
-console.log(b);0
+console.log(b);
 
 // //section 2
-// const grid = [
-//     ['X', 'o', 'x', 'X', 'o'],
-//     ['x', 'x', 'X', 'o', 'o'],
-//     ['X', 'o', 'o', 'x', 'X'],
-//     ['o', 'o', 'X', 'x', 'o'],
-//     ['X', 'x', 'o', 'o', 'X']
-// ];
+const grid = [
+    ['X', 'o', 'x', 'X', 'o'],
+    ['x', 'x', 'X', 'o', 'o'],
+    ['X', 'o', 'o', 'x', 'X'],
+    ['o', 'o', 'X', 'x', 'o'],
+    ['X', 'x', 'o', 'o', 'X']
+];
 
-// let cnt = 0;
-// for (let i = 0; i < grid.length; i++){
-//     for (let j = 0; j< grid[i].length; j++){
-//         if( grid[i][j] == "X"){
-//             cnt ++;
-//             console.log(`'X' found in [${i}][${j}]`);
-//         }
-//     }
-// }
-// console.log(`sum of all 'X': ${cnt}`);
+let cnt = 0;
+for (let i = 0; i < grid.length; i++){
+    for (let j = 0; j< grid[i].length; j++){
+        if( grid[i][j] == "X"){
+            cnt ++;
+            console.log(`'X' found in [${i}][${j}]`);
+        }
+    }
+}
+console.log(`sum of all 'X': ${cnt}`);
 
 // section 3
 let family = { parents: {}, children: [ { name: "Ali" }, { name: "Lea" }, { name: "Mona" } ] };
