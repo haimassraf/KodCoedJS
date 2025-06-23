89cc12f0b86bb9165983e71ba22e9b8b29cb8b52
Haim Assraf <haimassraf@gmail.com>
2025-06-23 12:07:25 +0300

    df

--- 23.06.25/function.js
+++ 23.06.25/function.js
@@ -1,21 +1,20 @@
-// arrow function
-const add = (num1, num2) =>  num1 + num2 
-const subtract = (num1, num2) =>  {
-   res = num1 - num2
-   return divide(res, 10)
-} 
+function start(num, fun){
+   return fun(num, 2)
+}
+
 const multiply = (num1, num2) =>  {
    res = num1 * num2
    return subtract(res, 5)
 } 
 
+const subtract = (num1, num2) =>  {
+   res = num1 - num2
+   return divide(res, 10)
+} 
+
 const divide = (num1, num2) =>  num1 / num2 
 
-
-
-function start(num, fun){
-   return fun(num, 2)
-}
+const add = (num1, num2) =>  num1 + num2 
 
 res = start(5, multiply)
 console.log(res);
